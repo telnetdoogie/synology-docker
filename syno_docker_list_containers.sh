7888f634960794ba83728cbd54488cf3c26d29a7
Jason Hobbs <telnetdoogie@gmail.com>
2024-11-03 14:59:58 -0600

    Update syno_docker_list_containers.sh

--- syno_docker_list_containers.sh
+++ syno_docker_list_containers.sh
@@ -57,8 +57,8 @@ if [ ${#docker_managed[@]} -gt 0 ]; then
 	echo
 	echo "The following containers may have been created with docker commands."
 	echo "Below are some clues on the command needed to to recreate them ONLY IF YOU HAVE NO OTHER WAY TO DO SO."
-	echo "...this is a best guess and may not be 100% accurate. If all containers already show as 'local' logger,"
- 	echo "there is no need to recreate."
+	echo "...this is a best guess and may not be 100% accurate. 
+        echo "If all containers already show as 'local' logger, there is no need to recreate them manually"
  	echo
 	for container in "${docker_managed[@]}"; do
 		docker_command=$(./container_recreate.sh $container)
