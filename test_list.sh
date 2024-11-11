442445a403fcb07ce3726a8c03299f487849740b
Jason Hobbs <telnetdoogie@gmail.com>
2024-11-11 16:38:01 -0600

    Removed inadvertent scratch file

--- test_list.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker inspect jellyfin --format "{{.Name}} {{if index .Config.Labels \"com.docker.compose.project.config_files\"}}{{index .Config.Labels \"com.docker.compose.project.config_files\"}}{{else}}CONFIG NOT FOUND{{end}} {{.HostConfig.LogConfig.Type}}"
